13688dc30c3fa52a970f344dc4c825647c7cdda5
habbkad <habibkadiri52@gmail.com>
2023-07-03 12:54:07 +0000

    Some buggs fixed

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-import { store } from "./Redux/Store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./Redux/Store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
